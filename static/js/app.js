c4998726444822a87c7584188e48dbaeadb03b09
Radhika-naik <radhikanaik302000@gmail.com>
2023-12-11 07:43:09 +0530

    Support for carotid thinning detection

--- static/js/app.js
+++ static/js/app.js
@@ -107,3 +107,52 @@ function selectImage() {
         }
     });
 }
+
+function selectImageCarotid() {
+    const imageInput = document.getElementById('imageInput');
+
+    // Trigger a click event on the hidden file input
+    imageInput.click();
+
+    // Add an event listener to handle the selected image
+    imageInput.addEventListener('change', function () {
+        const selectedImage = imageInput.files[0];
+        if (selectedImage) {
+            // Create a FormData object to send the image
+            const formData = new FormData();
+            formData.append('image', selectedImage);
+
+            // Replace the URL with your API endpoint
+            const apiUrl = '/uploadImage';
+
+            // Send a POST request to the API
+            fetch(apiUrl, {
+                method: 'POST',
+                body: formData,
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the API response data
+                console.log('API response:', data);
+
+                // Display the response using overlay pages instead of alerts
+                if (data.result) {
+                    var resultstr = data.result + "";
+                    console.log(typeof resultstr);
+                    console.log(resultstr);
+                    // Redirect to different overlay pages based on the API response
+                    if (resultstr === "Pneumonia Detected") {
+                        showOverlay('/static/images/carotid.jpg', 'Carotid artery thining detected! Please book a consultation with a doctor.', 'https://www.practo.com/bangalore/cardiologist', 'Book Consultation');
+                    } else {
+                        showOverlay('/static/images/happy_image.jpg', 'Carotid Artery is healthy!');
+                    }
+                } else if (data.error) {
+                    alert(data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        }
+    });
+}
